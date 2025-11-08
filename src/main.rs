7b043de55e6bde19954e5c05fb40e86f3f8cc325
songever <1021940593@qq.com>
2025-11-08 23:17:56 +0800

    Complete

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@
 123/0
 */
 
-use std::{borrow::Borrow, iter::Peekable, str::Chars};
+use std::{iter::Peekable, str::Chars};
 
 /* ENBF Grammar
 <Expr> ::= <AddSubExpr>                                    Priority |  LOW
@@ -19,11 +19,15 @@ use std::{borrow::Borrow, iter::Peekable, str::Chars};
 <PrimaryExpr> ::= NUM | '-'NUM |'('<Expr>')'                       \|/ HIGH
 */
 const DEBUG: i32 = 1;
-
+fn debug(msg: &str) {
+    if DEBUG != 0 {
+        println!("[debug]: {msg}");
+    }
+}
 // --- Lexer --- 
 enum TokenType
 {
-    NUMBER(u32),
+    NUMBER(i32),
     ADD,SUB,MUL,DIV,
     LEFTPAREN, RIGHTPAREN,
     END
@@ -42,35 +46,110 @@ impl<'a> Iterator for Lexer<'a> {
 
     fn next(&mut self) -> Option<Self::Item> {
         match self.src.next() {
-            Some('+') => Some(TokenType::ADD),
-            Some('-') => Some(TokenType::SUB),
-            Some('*') => Some(TokenType::MUL),
-            Some('/') => Some(TokenType::DIV),
-            Some('(') => Some(TokenType::LEFTPAREN),
-            Some(')') => Some(TokenType::RIGHTPAREN),
+            Some('+') => { debug("TOKEN:ADD"); Some(TokenType::ADD)},
+            Some('-') => { debug("TOKEN:SUB"); Some(TokenType::SUB)},
+            Some('*') => { debug("TOKEN:MUL"); Some(TokenType::MUL)},
+            Some('/') => { debug("TOKEN:DIV"); Some(TokenType::DIV)},
+            Some('(') => { debug("TOKEN:LEFTPAREN"); Some(TokenType::LEFTPAREN)},
+            Some(')') => { debug("TOKEN:RIGHTPAREN"); Some(TokenType::RIGHTPAREN)},
             Some(head@'0'..='9') => {
-                let mut num = head.to_digit(10).unwrap();
+                debug("TOKEN:NUMBER");
+                let mut num = head.to_digit(10).unwrap() as i32;
                 while let Some(&following@'0'..='9') = self.src.peek() {
-                    num = num * 10 + following.to_digit(10).unwrap();
+                    num = num * 10 + following.to_digit(10).unwrap() as i32;
                     self.src.next();
                 }
                 Some(TokenType::NUMBER(num))
             }
-            None => Some(TokenType::END),
+            None => {debug("TOKEN: END"); Some(TokenType::END) },
             _ => None,
         }
     }
 }
 // --- Parser & Interpreter --- (both in EvalXXX) 
+//<Expr> ::= <AddSubExpr>
+fn eval_expr(lexer: &mut Lexer) -> i32
+{
+    // Debug("EVAL:Expr");
 
+    eval_add_sub_expr(lexer)
+}
+
+//<AddSubExpr> ::= <MulDivExpr> {('+'|'-') <MulDivExpr>}
+fn eval_add_sub_expr(lexer: &mut Lexer) -> i32
+{
+    debug("EVAL:AddSubExpr");
+
+    let mut addsub_exprval = eval_mul_div_expr(lexer);
+    while let Some('+') | Some('-') = lexer.src.peek() // if have + or - go on
+    {
+        match lexer.next() {
+            Some(TokenType::ADD) => addsub_exprval += eval_mul_div_expr(lexer),
+            Some(TokenType::SUB) => addsub_exprval -= eval_mul_div_expr(lexer),
+            _ => unreachable!(),
+        }
+    }
+
+    addsub_exprval
+}
+
+//<MulDivExpr> ::= <PrimaryExpr> {('*'|'/') <PrimaryExpr>}
+fn eval_mul_div_expr(lexer: &mut Lexer) -> i32
+{
+    debug("EVAL:MulDivExpr");
+
+    let mut muldiv_exprval = eval_primary_expr(lexer);
+    while let Some('*') | Some('/') = lexer.src.peek() // if have * or / go on
+    {
+        match lexer.next() {
+            Some(TokenType::MUL) => muldiv_exprval *= eval_primary_expr(lexer),
+            Some(TokenType::DIV) => {
+                match eval_primary_expr(lexer) {
+                    0 => panic!("Division by zero"), 
+                    nonzero => muldiv_exprval /= nonzero,
+                };
+            }
+            _ => unreachable!(),
+        }
+    }
+    muldiv_exprval
+}
+
+//<PrimaryExpr> ::= NUM | '-'NUM | '('<Expr>')'
+fn eval_primary_expr(lexer: &mut Lexer) -> i32
+{
+    debug("EVAL:PrimaryExpr");
+
+    match lexer.next() {
+        Some(TokenType::NUMBER(num)) => num,
+        Some(TokenType::SUB) => {
+            if let Some('0'..='9') = lexer.src.peek() {
+                match lexer.next() {
+                    Some(TokenType::NUMBER(num)) => -num,
+                    _ => panic!("Negative sign without a number"),
+                }
+            } else {
+                panic!("Negative sign without a number");
+            }
+        },
+        Some(TokenType::LEFTPAREN) => {
+            let num = eval_expr(lexer);
+            match lexer.next() {
+                Some(TokenType::RIGHTPAREN) => num,
+                _ => panic!("Mismatched parentheses"),
+            }
+        }
+        _ => panic!("Illegal PrimaryExpr"),
+    }
+}
 
 // --- Driver ---
 fn main() {
     let mut src = String::new();
     std::io::stdin().read_line(&mut src).expect("Failed to read input");
-    
-    get_token();
-    let expr_val = EvalExpr();
+    let mut lexer = Lexer::from(src.as_ref());
+
+    let expr_val = eval_expr(&mut lexer);
     println!("\nEvaluate result :)");
     println!("{expr_val}");
 }
